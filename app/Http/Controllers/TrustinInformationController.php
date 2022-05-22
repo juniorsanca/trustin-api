13fceca9ba583d051a759ab3090b3de5a31245d4
junior SANCA <juniorsanca@MacBook-Pro-de-junior.local>
2022-05-22 15:34:15 +0200

    Controllers created: TrustinInformation, Rating, Creator

---
+++ app/Http/Controllers/TrustinInformationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TrustinInformation;
+use Illuminate\Http\Request;
+
+class TrustinInformationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TrustinInformation  $trustinInformation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TrustinInformation $trustinInformation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TrustinInformation  $trustinInformation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TrustinInformation $trustinInformation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TrustinInformation  $trustinInformation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TrustinInformation $trustinInformation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TrustinInformation  $trustinInformation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TrustinInformation $trustinInformation)
+    {
+        //
+    }
+}
