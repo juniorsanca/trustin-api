c635df210d73ee35501458da0701f4128f96616f
junior SANCA <juniorsanca@MacBook-Pro-de-junior.local>
2022-06-14 21:15:30 +0200

    user controller and model, logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\response;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -81,5 +83,38 @@ class UserController extends Controller
     public function destroy(User $user)
     {
         //
+    }
+
+    public function register(Request $request) 
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged Out'
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CreatorController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,9 +22,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/*----------[PUBLIC ROUTES]----------*/
+Route::post('/register', [UserController::class, 'register']);
+
+
+
+/*----------[PRIVATE ROUTES]----------*/
+Route::group(['middleware' => ['auth:sanctum']], function () {
 /*-----------[GET ALL CREATORS]-----------*/
 Route::get('creators', [CreatorController::class, 'index']);
 /*-------------[GET ONE CREATOR]-------------*/
 Route::get('/creators/{creator}', [CreatorController::class, 'show']);
 /*-------------[SEARCH ONE CREATOR]-------------*/
 Route::get('/creators/search/{name}', [CreatorController::class, 'search']);
+/*-------------[LOGGOUT]-----------*/
+Route::post('/logout', [UserController::class, 'logout']);
+
+});
