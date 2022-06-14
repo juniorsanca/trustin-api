ea9c6a830d0ccf12758311ce43488ebcc896df38
junior SANCA <juniorsanca@MacBook-Pro-de-junior.local>
2022-06-14 23:27:49 +0200

    register, login, logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -110,6 +110,35 @@ class UserController extends Controller
         return response($response, 201);
     }
 
+    public function login(Request $request) 
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+
+        ]);
+
+       // check email
+       $user = User::where('email', $fields['email'])->first();
+
+
+       //check password
+       if(!$user || !Hash::check($fields['password'], $user->password)){
+        return response([
+            'message' => 'Bad credentials'
+        ], 401);
+       }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
     public function logout(Request $request){
         auth()->user()->tokens()->delete();
 
--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 /*----------[PUBLIC ROUTES]----------*/
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
 
 
 
